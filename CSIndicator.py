813a6fb076c5e440faae9d929543b9c5c80debe2
jsseidel <jsseidel@fastmail.com>
2018-07-31 19:12:55 -0400

    Added ability to create new contexts and delete current context. Removed confirmation about creating new context.

--- CSIndicator.py
+++ CSIndicator.py
@@ -7,8 +7,6 @@
 #   + Make AppIndicator3 menu dynamic:
 #       https://askubuntu.com/questions/751608/how-can-i-write-a-dynamically-updated-panel-app-indicator
 #   + Convert contextswitch.sh Bash script to Python
-#   + Add ability to create new contexts in the AppIndicator3 menu
-#   + Add ability to delete contexts in the AppIndicator3 menu
 #############################################################################
 
 __all__ = ('CSIndicator')
@@ -30,6 +28,32 @@ from gi.repository import Notify
 from subprocess import Popen, PIPE
 
 APPINDICATOR_ID = 'ContextSwitcherIndicator'
+
+class CSCreateContextDialog(Gtk.Dialog):
+    def __init__(self, parent):
+        Gtk.Dialog.__init__(self, "Create Context", parent, 0, (Gtk.STOCK_CANCEL, Gtk.ResponseType.CANCEL, Gtk.STOCK_OK, Gtk.ResponseType.OK))
+        self.set_default_size(150, 100)
+        label = Gtk.Label("Context name:")
+        self.entry = Gtk.Entry()
+        self.entry.set_text("New context")
+        
+        box = self.get_content_area()
+        box.add(label)
+        box.add(self.entry)
+        self.show_all()
+
+    def get_text(self):
+        return self.entry.get_text()
+
+class CSConfirmDialog(Gtk.Dialog):
+    def __init__(self, parent, msg):
+        Gtk.Dialog.__init__(self, "Confirm", parent, 0, (Gtk.STOCK_NO, Gtk.ResponseType.NO, Gtk.STOCK_YES, Gtk.ResponseType.YES))
+        self.set_default_size(150, 100)
+        label = Gtk.Label(msg)
+        
+        box = self.get_content_area()
+        box.add(label)
+        self.show_all()
 
 class CSIndicator():
     def __init__(self, contextswitch_path):
@@ -54,13 +78,46 @@ class CSIndicator():
 
         menu_sep = Gtk.SeparatorMenuItem()
         menu.append(menu_sep)
-
+        
+        item_new = Gtk.MenuItem('New...')
+        item_new.connect('activate', self.new_context)
+        menu.append(item_new)
+        
+        item_del = Gtk.MenuItem('Delete...')
+        item_del.connect('activate', self.del_context)
+        menu.append(item_del)
+        
+        menu_sep = Gtk.SeparatorMenuItem()
+        menu.append(menu_sep)
+        
         item_quit = Gtk.MenuItem('Quit')
         item_quit.connect('activate', self.quit)
         menu.append(item_quit)
 
         menu.show_all()
         return menu
+
+    def del_context(self, menu_item):
+        (stdout, stderr) = self.run_command("cat " + os.environ['HOME'] + "/.contextswitch")
+        curr_context = stdout.decode("utf-8").rstrip()
+        del_dialog = CSConfirmDialog(None, "Are you sure you want to delete '" + curr_context + "'?")
+        response = del_dialog.run()
+        if response == Gtk.ResponseType.YES:
+            self.run_command("rm -rf " + os.environ['HOME'] + "/.config/contextswitch/" + curr_context)
+            self.run_command("rm -f " + os.environ['HOME'] + "/.contextswitch")
+            self.show_message(curr_context + " deleted. \n\nNo context is currently loaded.")
+
+        del_dialog.destroy()
+
+    def new_context(self, menu_item):
+        new_dialog = CSCreateContextDialog(None)
+        response = new_dialog.run()
+        if response == Gtk.ResponseType.OK:
+            (stdout, stderr) = self.run_command(self.contextswitch_path + " " + new_dialog.get_text())
+            self.show_message(stdout.decode("utf-8"))
+        elif response == Gtk.ResponseType.CANCEL:
+            print("Cancelled.")
+        new_dialog.destroy()
 
     def selection_made(self, menu_item):
         context = menu_item.get_label()
