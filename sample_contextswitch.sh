34f8ee25705624efca65dacd6c92d54a5ca1cb7f
jsseidel <jsseidel@fastmail.com>
2019-04-26 19:05:56 -0400

    Updated with better example

--- sample_contextswitch.sh
+++ sample_contextswitch.sh
@@ -4,40 +4,50 @@
 # This script should live in:
 #    ~/.config/contextswitch/<context_name>/contextswitch.sh
 #
-# The idea behind this script is that different contexts have
-# different workspaces, which are really just directories
-# containing:
+# The idea behind this script is that different contexts have different
+# workspaces, which are really just directories containing:
+#
 #   Desktop
 #   Documents
 #   Pictures
 #   git
 #
-# and so on. When you switch contexts, you first unload the old
-# context by removing the softlinks into the workspace directory.
+# and so on. When you switch contexts, you first unload the old context by
+# removing the softlinks into the workspace directory.
 #
 # You then load a new context by creating soft links into the
 # new workspace.
 #
-# This is just one way to handle changing directories using
-# the Gnome context switcher.
+# For this to work properly, you need to set up the softlinks exactly the way
+# they are expected to be here BEFORE you switch contexts, otherwise you get
+# things like links to the Desktop on your actual desktop.
+#
+# This is just one way to handle changing directories using the Gnome context
+# switcher.
 #
 
 CONTEXT=PersonalWorkspace
 
 if [[ "$1" == "load" ]] ; then
-    ln -s ~/$CONTEXT/git ~/git
-    ln -s ~/$CONTEXT/Documents ~/Documents
-    ln -s ~/$CONTEXT/Pictures ~/Pictures
-    # This is needed because nautilus will create a Desktop
-    # directory if there isn't one, and if we don't remove
-    # the empty directory, we end up with a link inside the
-    # Desktop directory.
-    if [[ -d ~/Desktop ]] ; then rmdir ~/Desktop ; fi
-    ln -s ~/$CONTEXT/Desktop ~/Desktop
+	ln -s ~/$CONTEXT/git ~/git
+	ln -s ~/Dropbox/Documents ~/Documents
+	ln -s ~/$CONTEXT/Pictures ~/Pictures
+	ln -s ~/$CONTEXT/Desktop ~/Desktop
+	ln -s ~/$CONTEXT/Music ~/Music
+	ln -s ~/$CONTEXT/Videos ~/Videos
 elif [[ "$1" == "unload" ]] ; then
-    rm -f ~/git
-    rm -f ~/Documents
-    rm -f ~/Pictures
-    rm -f ~/Desktop
+	rm -f ~/git
+	rm -f ~/Documents
+	rm -f ~/Pictures
+	rm -f ~/Desktop
+	rm -f ~/Music
+	rm -f ~/Videos
 fi
+
+# We kill nautilus-desktop here because we switched backgrounds
+# and Desktop directories
+killall nautilus-desktop
+sleep 2
+nohup nautilus-desktop --new-window </dev/null 1>/dev/null 2>&1 &
+
 
